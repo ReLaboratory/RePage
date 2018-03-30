8f5cf19a86a190c68eb77f659f3021498475a929
Junmo <yunedoopea610@gmail.com>
2018-03-30 20:05:08 +0900

    Modify way to scroll when user click move to button

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-
+  
   var scrollDownIcon = document.getElementsByClassName('header__scroll-down-icon');
-  var main= document.getElementsByClassName('main');
   scrollDownIcon[0].onclick = function() {
-    main[0].scrollIntoView({behavior: 'smooth'});
+    document.getElementsByClassName('section--introduce')[0].scrollIntoView({behavior: 'smooth'});
   }
+
 }
