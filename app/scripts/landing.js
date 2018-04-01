5145b9d266edc3b4284fbaea7d6ad419dd94c228
Junmo <yunedoopea610@gmail.com>
2018-04-01 22:30:11 +0900

    Add intruduce-reliablers section unperfected style, animation

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -2,7 +2,7 @@ window.onload = function() {
   
   var scrollDownIcon = document.getElementsByClassName('header__scroll-down-icon');
   scrollDownIcon[0].onclick = function() {
-    document.getElementsByClassName('section--introduce')[0].scrollIntoView({behavior: 'smooth'});
+    document.getElementsByClassName('section')[0].scrollIntoView({behavior: 'smooth'});
   }
 
 }
