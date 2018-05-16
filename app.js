89d1cedb7eee4823bbb9164babecc0d8b2202a75
Junmo <yunedoopea610@gmail.com>
2018-05-16 19:48:01 +0900

    Add Education style

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-var educationRouter = require('./routes/education');
+// var educationRouter = require('./routes/education'); temporarily
 
 var app = express();
 
@@ -19,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-app.use('/education', educationRouter);
+// app.use('/education', educationRouter); temporarily
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
