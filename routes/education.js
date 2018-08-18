28d71c5f002147ac131874d036ad1d184d546da8
WonJunKim <kimjnsjwj@gmail.com>
2018-08-18 15:32:59 +0900

    Change String-titles to Array-titles & index

--- routes/education.js
+++ routes/education.js
@@ -7,19 +7,19 @@ router.get('/:subject/:no', async (req, res) => {
   const { subject, no } = req.params;
 
   try {
-    const subjectObj = await Subject.findOne({ name: subject, lectures: { $elemMatch: {no: no}} });
-    if (subjectObj == null)
+    const subjectObj = await Subject.findOne({ name: subject });
+    if (subjectObj == null || subjectObj.lectures[no] == null)
       return res.status(404).end();
-
+      
     res.status(200).json({
-      index: no,
-      titles: subjectObj.lectures[no - 1].title,
+      index: no - 1,
+      titles: subjectObj.lectures.map(e => e.title),
       content: subjectObj.lectures[no - 1].content,
       src: subjectObj.lectures[no - 1].src
     });
   } catch (err) {
     console.log(err);
-    res.status(500).end('error!');
+    res.status(500).end();
   }
 });
 
