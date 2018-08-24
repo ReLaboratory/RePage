e0d150cb9fd1632c556fedab0cfe54b35d728e7f
WonJunKim <kimjnsjwj@gmail.com>
2018-08-24 19:19:26 +0900

    Fix file reading

--- routes/education.js
+++ routes/education.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const path = require('path');
 const Subject = require('../model/subject');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  require('fs').readFile('../public/lecture.html', (err, data) => {
+  require('fs').readFile(path.join(__dirname + '/../public/lecture.html'), (err, data) => {
     if(err)
     {
       console.log(err);
       return res.status(500).end();
     }
+    res.setHeader('content-type', 'text/plain')
     res.status(200).send(data);
   });
 });
