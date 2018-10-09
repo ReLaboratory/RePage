f35fd27df9c8e846c75ffc51907501452a2c0bcf
WonJunKim <kimjnsjwj@gmail.com>
2018-10-09 21:42:25 +0900

    fix

--- routes/education.js
+++ routes/education.js
@@ -3,29 +3,6 @@ const path = require('path');
 const Subject = require('../model/subject');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  require('fs').readFile(path.join(__dirname + '/../public/lecture.html'), (err, data) => {
-    if(err)
-    {
-      console.log(err);
-      return res.status(500).end();
-    }
-    res.setHeader('content-type', 'text/plain')
-    res.status(200).send(data);
-  });
-});
-
-router.get('/:subject', async (req, res) => {
-  const subject = req.params.subject;
-
-  const subjectObj = await Subject.findOne({ name: subject });
-  if(subjectObj == null)
-    return res.status(404).end();
-    res.status(200).json({
-      titles: subjectObj.lectures.map(e => e.title)
-    });
-});
-
 router.get('/:subject/:no', async (req, res) => {
   const { subject, no } = req.params;
 
@@ -36,6 +13,7 @@ router.get('/:subject/:no', async (req, res) => {
     
     res.status(200).json({
       index: no - 1,
+      titles: subjectObj.lectures.map(e => e.title),
       content: subjectObj.lectures[no - 1].content,
       src: subjectObj.lectures[no - 1].src
     });
