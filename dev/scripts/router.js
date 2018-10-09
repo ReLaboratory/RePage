3a4c58e9ab5580a4b963e4d34a5d96fd89fe5b5d
Junmo <yunedoopea610@gmail.com>
2018-10-09 12:08:50 +0900

    Switch router as hash instead pjax
    because of the window object doesn't provide path change event.
    so pjax can't respond refresh

--- dev/scripts/router.js
+++ dev/scripts/router.js
@@ -23,40 +23,25 @@
   }
 
   const routes = {
-    '/'() {
+    ''() {
       get('/data/landing.html').then(render);
     },
-    '/select'() {
+    'select'() {
       get('/data/select.html').then(render);
     },
-    '/lecture'() {
+    'lecture'() {
       get('/data/lecture.html').then(render);
     },
-    otherwise(path) {
-      app.innerHTML = `${path} Not Found`;
+    otherwise(hash) {
+      app.innerHTML = `${hash} Not Found`;
     }
   };
 
-  function router(path) {
-    (routes[path] || routes.otherwise)(path);
+  function router() {
+    const hash = location.hash.substr(1);
+    (routes[hash] || routes.otherwise)(hash);
   }
 
-  window.addEventListener('popstate', e => {
-    console.log('[popstate]', e.state);
-    // 이전페이지 / 다음페이지 button이 클릭되면 router를 호출
-    router(e.state.path);
-  });
-
-  navigation.addEventListener('click', e => {
-    if (!e.target || !e.target.classList.contains('link')) return;
-    e.preventDefault();
-    console.log('hello');
-    const path = e.target.getAttribute('href');
-
-    history.pushState({ path }, null, path);
-    router(path);
-  });
-
-  // 웹페이지가 처음 로딩되었을 때
-  router('/');
+  window.addEventListener('hashchange', router);
+  window.addEventListener('DOMContentLoaded', router);
 })();
